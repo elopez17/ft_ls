6c679b6c26506216eca371b5644277e0aa8ff037
Eric Lopez <eLopez@Erics-Mac.local>
2017-10-20 20:22:04 -0700

    organize

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: eLopez <elopez@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 14:26:09 by eLopez            #+#    #+#             */
-/*   Updated: 2017/10/20 09:50:41 by eLopez           ###   ########.fr       */
+/*   Updated: 2017/10/20 20:20:58 by eLopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,22 +47,45 @@ typedef struct	s_dirs
 	struct s_dirs	*next;
 }				t_dirs;
 
-void	ft_p_exit(char *s);
-void	ls_ill_opt(char c);
+/*
+**	handle directory & file arguements & default routine.
+*/
 void	ls_pathiter(t_option *opt, char *files, char **path);
 void	ls_path(t_option *opt, char *files, char *path);
+
+/*
+**	Get info functions
+*/
 char	*ls_get_files(char *files, char *file, t_option *opt);
 char	*ls_get_dirs(char *path, char *dir);
 void	ls_insert_dirs(t_dirs *new, t_dirs **dir, unsigned int rev);
+time_t	ls_get_time(char *dir_path, char *file);
+
+/*
+**	Sort files using quicksort implementation.
+*/
 char	**ls_sort_files(t_dirs *d, char *files, char *dir_path, t_option *opt);
 void	ls_sort(char **files, int start, int end);
 void	timesort(char **files, int start, int end, char *dir_path);
 void	ls_swap_files(char **s1, char **s2);
-time_t	ls_get_time(char *dir_path, char *file);
+
+/*
+**	Printing functions
+*/
 void	ls_print_data(char **files, char *path, int width, t_option *opt);
 void	ls_print_perm(int octal);
+
+/*
+**	free memory functions
+*/
 void	free_2d(char ***arr_2d);
 void	ls_free_lst(t_dirs **head);
 void	ls_free_all(char **files, char **dirs, char ***path, t_option **opt);
+
+/*
+**	Error functions
+*/
+void	ft_p_exit(char *s);
+void	ls_ill_opt(char c);
 
 #endif
